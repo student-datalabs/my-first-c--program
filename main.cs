b21b3de2567c6683b5daa7b293b096f38a8ca15c
student-datalabs <159149233+student-datalabs@users.noreply.github.com>
2024-02-09 00:37:03 +0200

    Create main.cs
    No comment!

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Gemeric;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+   {
+      class Programm
+      { 
+           static void Main(string[] args)
+           {
+              string message = "Hello World!!";
+
+              Console.WriteLine(message);
+           }
+      }
+}
